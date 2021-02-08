db78d32ae220f6b9e0f934eda06373282337b9f3
ppppkkkk <45647667+ppppkkkk@users.noreply.github.com>
2021-02-08 20:07:29 +0800

    Add files via upload

---
+++ P6_B1001.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+int main()
+{
+	int step = 0, n;
+	scanf_s("%d", &n);
+	while (n != 1)
+	{
+		if (n % 2 == 0) n = n / 2;
+		else n = (3 * n + 1) / 2;
+		step++;
+	}
+	printf("%d\n", step);
+	return 0;
+}
