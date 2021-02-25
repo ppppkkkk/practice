bd1ffc95a8c267242ed17d1429093988491b1ffc
ppppkkkk <45647667+ppppkkkk@users.noreply.github.com>
2021-02-25 17:48:07 +0800

    Create P13_B1012.cpp

---
+++ 3.1/P13_B1012.cpp
@@ -0,0 +1,54 @@
+
+//P13_B1012
+
+#include <stdio.h>
+int main()
+{
+	int count[5] = { 0 };
+	int ans[5] = { 0 };
+	int n,temp;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &temp);
+		if (temp % 5 == 0)
+		{
+			ans[0] += temp;
+			count[0]++;
+		}
+		else if (temp % 5 == 1) {
+			if (count[1] % 2 == 0) {
+				ans[1] += temp;
+			}
+			else {
+				ans[1] -= temp;
+			}
+			count[1]++;
+		}
+		else if (temp % 5 == 2) {
+			count[2]++;
+		}
+		else if (temp%5==3){
+			ans[3] += temp;
+			count[3]++;
+		}
+		else
+		{
+			if (temp > ans[4]) {
+				ans[4] = temp;
+			}
+			count[4]++;
+		}
+	}
+	if (count[0] == 0)printf("N ");
+	else printf("%d ", ans[0]);
+	if (count[1] == 0)printf("N ");
+	else printf("%d ", ans[1]);
+	if (count[2] == 0)printf("N ");
+	else printf("%d ", ans[2]);
+	if (count[3] == 0)printf("N ");
+	else printf("%d ", ans[3]);
+	if (count[4] == 0)printf("N");
+	else printf("%d", ans[4]);
+	return 0;
+}
